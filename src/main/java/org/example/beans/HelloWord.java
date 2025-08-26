3bebf92a2007fa2200e22da27d0476f4d53cd2e6
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-08-26 00:22:38 -0600

    Charged(Configured) a bean to context

---
+++ src/main/java/org/example/beans/HelloWord.java
@@ -0,0 +1,13 @@
+package org.example.beans;
+
+public class HelloWord {
+
+    private String greeting;
+
+    public String getGreeting(){
+        return greeting;
+    }
+    public void setGreeting(String greeting){
+        this.greeting = greeting;
+    }
+}
